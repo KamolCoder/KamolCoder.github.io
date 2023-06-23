042ec34af8954815a6a0d10016781afa34013612
KamolCoder <121795348+KamolCoder@users.noreply.github.com>
2023-06-23 18:16:22 +0500

    Update app.js

--- app.js
+++ app.js
@@ -6,11 +6,14 @@ tg.MainButton.textColor = '#FFFFFF';
 tg.MainButton.color = '#2cab37';
 
 let room = "";
+let hour = "";
 
 let room1 = document.getElementById('room1');
 let room2 = document.getElementById('room2');
 let room3 = document.getElementById('room3');
 let room4 = document.getElementById('room4');
+let plus = document.getElementById('plus');
+let minus = document.getElementById('minus');
 
 room1.addEventListener('click', function(){
     if (tg.MainButton.isVisible) {
@@ -53,9 +56,20 @@ room4.addEventListener('click', function(){
     }
 });
 
+let myData = 0;
+let myHeading = document.getElementById('myHeading');
+myHeading.innerHTML = myData;
+
+plus.addEventListener('click',function(){
+    myData +=1
+})
+
+minus.addEventListener('click',function(){
+    myData -=1
+})
 
 
 
 Telegram.WebApp.onEvent('mainButtonClicked',function(){
-    tg.sendData(room);
+    tg.sendData(room+"_"+hour);
 });
