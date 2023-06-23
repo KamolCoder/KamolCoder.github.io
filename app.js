64899b1f7642267a6f900ed1750e0f0b2949fc3e
KamolCoder <121795348+KamolCoder@users.noreply.github.com>
2023-06-23 17:35:31 +0500

    Update app.js

--- app.js
+++ app.js
@@ -5,13 +5,54 @@ tg.expand()
 tg.MainButton.textColor = '#FFFFFF';
 tg.MainButton.color = '#2cab37';
 
-let room = document.getElementById("choose").value;
-let hours = document.getElementById("numberinput").value;
-let datetime = document.getElementById("start").value;
-let data = room + "_" + hours + "_" + datetime;
+let room = "";
+let room1 = document.getElementById('room1');
+let room2 = document.getElementById('room2');
+let room3 = document.getElementById('room3');
+let room4 = document.getElementById('room4');
 
-tg.MainButton.show()
-Telegram.WebApp.onEvent('mainButtonClicked',function(){
-    tg.sendData(data);
+room1.addEventListener('click', function(){
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Выбрано комната - WorkSpace ");
+        room = '1';
+        tg.MainButton.show();
+    }
 });
 
+room2.addEventListener('click', function(){
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Выбрано комната - IventZone ");
+        room = '2';
+        tg.MainButton.show();
+    }
+});
+room3.addEventListener('click', function(){
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Выбрано комната - BigMiting ");
+        room = '3';
+        tg.MainButton.show();
+    }
+});
+room4.addEventListener('click', function(){
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Выбрано комната - SmallMiting ");
+        room = '4';
+        tg.MainButton.show();
+    }
+});
+
+Telegram.WebApp.onEvent('mainButtonClicked',function(){
+    tg.sendData(room);
+});
