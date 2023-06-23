00cf97bcf42f69331c4a8cb354f1bd8946bea931
KamolCoder <121795348+KamolCoder@users.noreply.github.com>
2023-06-23 13:27:19 +0500

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ tg.MainButton.textColor = '#FFFFFF';
 tg.MainButton.color = '#2cab37';
 
 let item = 'proverka js';
-
+tg.MainButton.show()
 Telegram.WebApp.onEvent('mainButtonClicked',function(){
     tg.sendData(item);
 });
